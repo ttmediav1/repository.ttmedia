f37a8b33eaaf920597e68cbb997bbdb9393cc1b7
ttmediav1 <eng64tw@gmail.com>
2018-02-14 18:52:56 -0800

    3.4
    3.4

--- plugin.video.tempest/default.py
+++ plugin.video.tempest/default.py
@@ -22,8 +22,8 @@ import __builtin__
 # -----------------------
 # change these to suit your addons
 root_xml_url = "http://ttmedia.live/tempest/main.xml"  # url of the root xml file
-__builtin__.tvdb_api_key = "0629B785CE550C8D"  # tvdb api key
-__builtin__.tmdb_api_key = "66e34e89e655538fecd4aca6dc820141"  # tmdb api key
+__builtin__.tvdb_api_key = "146E7EB2B9C689B2"  # tvdb api key
+__builtin__.tmdb_api_key = "63aa68b8717a7477ff33c5a0ffa31355"  # tmdb api key
 __builtin__.trakt_client_id = "948c4e2514db4f2cca33e737f5e73ae213e537d344b9916983e53e3cc2a1b21c"  # trakt client id
 __builtin__.trakt_client_secret = "140369c2c981732a7b183db2e9300065629b8b7417a149a0c4e098ebee8f692e"  # trakt client secret
 __builtin__.search_db_location = ""  # location of search db
