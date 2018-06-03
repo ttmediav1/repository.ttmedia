a3168f5761384d3d8e913e8c31ad17bcc2126244
ttmediav1 <eng64tw@gmail.com>
2018-06-02 23:08:53 -0700

    updated 8.0 tempest updated repo 1.4.5
    updated 8.0 and 1.4.5

---
+++ plugin.video.btmedia/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLmJ0bWVkaWE=')
+
+name = b.b64decode('Qk9TUy1URUNIIE1lZGlh')
+
+host = b.b64decode('aHR0cDovL2xpbWl0bGVzcy1pcHR2LmlzLWZvdW5kLm9yZw==')
+
+port = b.b64decode('MjU0NjE=')
