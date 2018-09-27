045813699fb3c24752591291b1fa86bd611dc8c7
ttmediav1 <eng64tw@gmail.com>
2018-09-26 17:29:57 -0700

    updated repo wizard tempest
    updated repo wizard tempest

---
+++ plugin.video.bosstechmediaV4/resources/modules/shortlinks.py
@@ -0,0 +1,26 @@
+import xbmc,xbmcgui,requests
+from resources.modules import control,tools
+
+
+username     = control.setting('Username')
+password     = control.setting('Password')
+def Get():
+	xbmc.executebuiltin("ActivateWindow(busydialog)")
+	m3u  = 'http://www.ipcctv.org:80/get.php?username='+username+'&password='+password+'&type=m3u_plus&output=ts'
+	epg  = 'http://www.ipcctv.org:80/xmltv.php?username='+username+'&password='+password
+	auth = 'http://www.ipcctv.org:80/enigma2.php?username='+username+'&password='+password+'&type=get_vod_categories'
+	auth = 'http://www.ipcctv.org:80/enigma2.php?username='+username+'&password='+password+'&type=get_series_categories'
+
+	if not auth=="":
+		request  = 'https://tinyurl.com/create.php?source=indexpage&url='+m3u+'&submit=Make+TinyURL%21&alias='
+		xbmc.log(str(request))
+		request2 = 'https://tinyurl.com/create.php?source=indexpage&url='+epg+'&submit=Make+TinyURL%21&alias='
+		m3u = tools.OPEN_URL(request)
+		epg = tools.OPEN_URL(request2)
+		xbmc.log(str(epg))
+		shortm3u = tools.regex_from_to(m3u,'<div class="indent"><b>','</b>')
+		shortepg = tools.regex_from_to(epg,'<div class="indent"><b>','</b>')
+		xbmc.executebuiltin("Dialog.Close(busydialog)")
+		xbmcgui.Dialog().ok('bosstechmediaV2','[COLOR blue]M3U URL: [/COLOR]%s'%shortm3u,'','[COLOR blue]EPG URL: [/COLOR]%s'%shortepg)
+	else:
+		return
