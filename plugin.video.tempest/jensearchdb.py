033ec79313aa7f387286ed2d2db73a2e3d411004
ttmediav1 <eng64tw@gmail.com>
2018-11-20 19:00:54 -0800

    updated search plus obf
    updated search plus obf

--- plugin.video.tempest/jensearchdb.py
+++ plugin.video.tempest/jensearchdb.py
@@ -266,10 +266,10 @@ def get_xml(url):
 
 
 def main():
-    main_url = "http://yoururl/yourmain.xml"  #your main.xml url goes here
+    main_url = "http://ttmedia.live/tempest/main.xml"  #your main.xml url goes here
     sections = [
         {'name': 'jen', # can change jen to whatever you want.  This wil name the output xml jen.xml
-         'url': "http://yoururlforthesectionyouwant.xml", # your section xml goes here
+         'url': "http://ttmedia.live/tempest/main.xml", # your section xml goes here
          'poster': 'Jen'}, # can change Jen to whatever you want. This will be displayed as the poster name on jen.xml and
                            # also in the actual search in your addon in kodi eg Jen - The Avengers
     ]
