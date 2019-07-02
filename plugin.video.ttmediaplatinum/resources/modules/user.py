e8260e96e6a992bd6dda771a9fd6b5d4c5253321
ttmediav1 <eng64tw@gmail.com>
2019-07-01 23:35:54 -0700

    updated repo
    updated repo

---
+++ plugin.video.ttmediaplatinum/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLmlwdHY=')
+
+name = b.b64decode('SVBUVg==')
+
+host = b.b64decode('aHR0cDovL29udGFyZ2V0LmxpdmU=')
+
+port = b.b64decode('ODA4MA==')
