dd2025c7e7aeeb40671a056db7670a49a7345780
ttmediav1 <eng64tw@gmail.com>
2019-07-29 23:47:05 -0700

    added tools py
    added tools py

--- plugin.video.tempest/resources/lib/plugins/tools.py
+++ plugin.video.tempest/resources/lib/plugins/tools.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 """
-
-    Copyright (C) 2018 MuadDib
+    tools.py --- Jen Plugin for accessing various tools
+    Copyright (C) 2018: MuadDib, Jen
 
     ----------------------------------------------------------------------------
     "THE BEER-WARE LICENSE" (Revision 42):
@@ -14,13 +15,18 @@
         Drop this PY in the plugins folder, and use whatever tools below you want.
 
     Version:
+        2019-07-25
+            - Added option to authorize ResolveURL with Premiumize.me debrid service (see Usage Examples below)
+            - Added option to authorize ResolveURL with LinkSnappy debrid service (see Usage Examples below)
+            - Added option to clear Resolver Function Cache for ResolveURL and/or URLResolver (see Usage Examples below)
+            - Categorized and added Usage Examples
         2018.11.17
             - Updated the PAIR_LIST (replaced the_video_me with vevio; replaced vid_up_me with vidup, replaced vshare with videoshare)
                 - updated URL for openload and vidup
-            - Added AUTH_LIST to authorize ResolveURL with RealDebrid or AllDebrid 
+            - Added AUTH_LIST to authorize ResolveURL with RealDebrid or AllDebrid
                 - added <authwith> tags
-                - can use authlist to display both debrid services, or specific entry from AUTH_LIST to authorize only that service
-            - Added customizable settings for 2 Colors for the header & list of items displayed in the Pair/Authorize windows
+                - can use "authlist" to display both debrid services, or specific entry from AUTH_LIST to authorize only that service
+            - Added customizable settings for 2 COLORS for the Header & list of Items displayed in the Pair/Authorize/Function Cache windows
 
         2018.7.14
             - Updated password code to cache a session for X amt of time
@@ -48,19 +54,19 @@
             - Initial Release
 
     XML Explanations:
-        Tags: 
+        Tags:
             <heading></heading> - Displays the entry as normal, but performs no action (not a directory or "item")
             <mysettings>0/0</mysettings> - Opens settings dialog to the specified Tab and section (0 indexed)
-            <pairwith></pairwith> - Used for pairing with sites. See list below of supported sites with this plugin
-            <authwith></authwith> - Used for authorizing Debrid services (RealDebrid, AllDebrid).
+            <pairwith></pairwith> - Used for pairing with sites. See list of supported sites on line 210 below
+            <authwith></authwith> - Used for authorizing Debrid services (RealDebrid, AllDebrid, Premiumize, LinkSnappy)
             <passreq></passreq> - Used to password protect submenus. Format is base64 encoded string formatted like this:
-                Password|link_to_xml
-            <trailer>plugin://plugin.video.youtube/play/?video_id=ChA0qNHV1D4</trailer>
-            
+                                    Password|link_to_xml
+            <trailer>plugin://plugin.video.youtube/play/?video_id=ChA0qNHV1D4</trailer> - Adds Trailer option for this movie in the context menu when Metadata is DISABLED in your addon
+
 
     *** COLORS ***
-        Set your desired colors for COLOR1 & COLOR2 within "" on lines 144 & 145 below.
-        COLOR1 is for the header & COLOR2 is for the items list, displayed in the Pair/Authorize window.
+        Set your desired colors for COLOR1 & COLOR2 within "" on lines 206 & 207 below.
+        COLOR1 is for the Header & COLOR2 is for the Items list, displayed in the Pair/Authorize/Function Cache window.
         The color values can be alphanumeric (example: red, limegreen) or Hex (example: ffff0000, FF00FF00).
         If colors are left blank, they will display as the default color set within the skin you're using.
 
@@ -68,71 +74,117 @@
 
     Usage Examples:
 
-        <item>
-            <title>[COLOR limegreen]Don't forget to folow me on twitter @tantrumdev ![/COLOR]</title>
-            <heading></heading>
-        </item>
+	### Settings ###
 
+        ** Open the Settings for the addon on the Customization tab
         <item>
             <title>JEN: Customization</title>
             <mysettings>0/0</mysettings>
-            <info>Open the Settings for the addon on the Customization tab</info>
         </item>
 
+        ** Open the Settings for the addon on the Cache tab
+
         <item>
-            <title>Pair With Sites</title>              ### Gives option to pair device with any of the pairing sites in the PAIR_LIST on line 156 below ### 
+            <title>JEN: Cache</title>
+            <mysettings>1/0</mysettings>
+        </item>
+
+	### Pairing ###
+
+        ** Gives option to pair device with any of the supported pairing sites
+        <item>
+            <title>Pair With Sites</title>
             <pairwith>pairlist</pairwith>
         </item>
 
+        ** Opens Openload site to pair device with.  To paith with a site other than "openload", use "flashx", "streamango", "streamcherry", "vevio", "vidup" or "videoshare"
         <item>
-            <title>Pair Openload</title>                ### Opens Openload site to pair device with ###
-            <pairwith>openload</pairwith>               ### To paith with a site other than "openload", use any of the sites listed in the PAIR_LIST on line 156 below
+            <title>Pair Openload</title>
+            <pairwith>openload</pairwith>
         </item>
 
+	### Authorizing ###
+
+        ** Gives option to authorize ResolveURL with any/all supported Debrid Services
         <item>
-            <title>Authorize Debrid</title> 
-            <authwith>authlist</authwith>               ### Gives option to authorize ResolveURL with either AllDebrid or RealDebrid service ###
+            <title>Authorize Debrid Services</title>
+            <authwith>authlist</authwith>
         </item>
 
+        ** Displays code to authorize ResolveURL with RealDebrid, waits 20 seconds, then displays the RealDebrid site to enter the code in
         <item>
             <title>Authorize RealDebrid</title>
-            <authwith>realdebrid</authwith>             ### Displays code to authorize ResolveURL with RealDebrid, then displays the RealDebrid site to enter the code in ###
+            <authwith>realdebrid</authwith>
         </item>
 
+        ** Displays code to authorize ResolveURL with AllDebrid, waits 20 seconds, then displays the AllDebrid site to enter the code in
         <item>
             <title>Authorize AllDebrid</title>
-            <authwith>alldebrid</authwith>              ### Displays code to authorize ResolveURL with AllDebrid, then displays the AllDebrid site to enter the code in ###
+            <authwith>alldebrid</authwith>
         </item>
 
+        ** Displays code to authorize ResolveURL with Premiumize.me, waits 20 seconds, then displays the Premiumize.me site to enter the code in
+        <item>
+            <title>Authorize Premiumize</title>
+            <authwith>premiumize</authwith>
+        </item>
+
+        ** Displays prompt for LinkSnappy account details (Username, Password) to authorize ResolveURL with LinkSnappy
+        <item>
+            <title>Authorize LinkSnappy</title>
+            <authwith>linksnappy</authwith>
+        </item>
+
+	### Function Cache ###
+
+        ** Gives option to clear function cache any/all supported Resolvers
+        <item>
+            <title>Clear Resolver Function Cache</title>
+            <funccache>funclist</funccache>
+        </item>
+
+        ** Clears function cache for ResolveURL
+        <item>
+            <title>Clear ResolveURL Function Cache</title>
+            <funccache>resolveurl</funccache>
+        </item>
+
+        ** Clears function cache for URLResolver
+        <item>
+            <title>Clear URLResolver Function Cache</title>
+            <funccache>urlresolver</funccache>
+        </item>
+
+	### Password Protection ###
+
+        ** Password protects a menu for which its corresponding xml is a Local file.  Everything between the <passreq></passreq> tags MUST be Base64 Encoded!
         <item>
             <title>Password Protected Local File</title>
-            <passreq>VGhpc0lzVGhlUGFzc3dvcmR8ZmlsZTovL3N1Ym1lbnUueG1s</passreq>
+            <passreq>ThisIsThePassword|file://submenu.xml</passreq>
         </item>
 
+        ** Password protects a menu for which its corresponding xml is a Remote file  Everything between the <passreq></passreq> tags MUST be Base64 Encoded!
         <item>
             <title>Password Protected Remote File</title>
-            <passreq>VGhpc0lzVGhlUGFzc3dvcmR8aHR0cDovL3d3dy5leGFtcGxlLmNvbS9zdWJtZW51LnhtbA==</passreq>
+            <passreq>ThisIsThePassword|http://www.example.com/submenu.xml</passreq>
         </item>
 
+	### Trailer ###
+
+        ** Adds the Trailer option for a specific movie to the context menu when Metadata is DISABLED in your addon
         <item>
             <title>Dune (1984)</title>
             <trailer>plugin://plugin.video.youtube/play/?video_id=ChA0qNHV1D4</trailer>
-            <info>Provides the Trailer context link for this movie when Metadata is DISABLED in your addon.</info>
         </item>
 
-        <item>
-            <title>JEN: General</title>
-            <mysettings>1/0</mysettings>
-            <info>Open the Settings for the addon on the General tab</info>
-        </item>
+	### Custom Modes ###
 
+        ** Sets a specific Mode for the menu item, to utilize Jen modes not normally accessible. Setting modeurl passes a custom built url= variable to go with it
         <item>
             <title>Custom Mode</title>
             <mode>Whatever</mode>
             <modeurl>query=Iwant</modeurl>
-            <info>Sets a specific Mode for the menu item, to utilize Jen modes not normally accessible. Setting modeurl passes a custom built url= variable to go with it</info>
         </item>
-
 
 """
 
@@ -156,16 +208,22 @@ COLOR2 = ""
 
 
 PAIR_LIST = [ ("flashx", "https://www.flashx.tv/?op=login&redirect=https://www.flashx.tv/pairing.php"),
-        ("openload", "https://olpair.com"),
-        ("streamango", "https://streamango.com/pair"),
-        ("streamcherry", "https://streamcherry.com/pair"),
-        ("vevio", "https://vev.io/pair"),
-        ("vidup", "https://vidup.io/pair"),
-        ("videoshare", "http://vshare.eu/pair") ]
+              ("openload", "https://olpair.com"),
+              ("streamango", "https://streamango.com/pair"),
+              ("streamcherry", "https://streamcherry.com/pair"),
+              ("vevio", "https://vev.io/pair"),
+              ("vidup", "https://vidup.io/pair"),
+              ("videoshare", "http://vshare.eu/pair") ]
 
 
 AUTH_LIST = [ ("realdebrid", "http://real-debrid.com/device"),
-        ("alldebrid", "https://alldebrid.com/pin") ]
+              ("alldebrid", "https://alldebrid.com/pin"),
+              ("premiumize", "https://www.premiumize.me/device"),
+              ("linksnappy", "https://linksnappy.com") ]
+
+
+FUNC_LIST = [ ("resolveurl", "script.module.resolveurl"),
+              ("urlresolver", "script.module.urlresolver") ]
 
 
 class JenTools(Plugin):
@@ -308,6 +366,25 @@ class JenTools(Plugin):
             }
             result_item["info"]["trailer"] = item.get("trailer", None)
             return result_item
+        elif "<funccache>" in item_xml:
+            item = JenItem(item_xml)
+            result_item = {
+                'label': item["title"],
+                'icon': item.get("thumbnail", addon_icon),
+                'fanart': item.get("fanart", addon_fanart),
+                'mode': "FUNCCACHE",
+                'url': item.get("funccache", ""),
+                'folder': False,
+                'imdb': "0",
+                'content': "files",
+                'season': "0",
+                'episode': "0",
+                'info': {},
+                'year': "0",
+                'context': get_context_items(item),
+                "summary": item.get("summary", None)
+            }
+            return result_item
 
 
 @route(mode='HEADING')
@@ -337,7 +414,7 @@ def password_handler(url):
     sep_list = url.decode('base64').split('|')
     dec_pass = sep_list[0]
     xml_loc = sep_list[1]
-    
+
     SESSION_HOURS = this_addon.getSetting('SESSION_HOURS')
     if SESSION_HOURS == '':
         SESSION_HOURS = '1'
@@ -413,7 +490,7 @@ def pairing_handler(url):
                     break
 
         check_os = platform()
-        if check_os == 'android': 
+        if check_os == 'android':
             open_site = xbmc.executebuiltin('StartAndroidActivity(,android.intent.action.VIEW,,%s)' % (site))
         elif check_os == 'osx':
            os.system("open " + site)
@@ -426,7 +503,7 @@ def pairing_handler(url):
 
 
 @route(mode="AUTHWITH", args=["url"])
-def authRD(url):
+def authorizing_handler(url):
     try:
         site = ''
         if 'authlist' in url:
@@ -452,17 +529,58 @@ def authRD(url):
                     break
         if str(item[0]) == "realdebrid":
             auth_mode = "auth_rd"
-        else:
+        elif str(item[0]) == "alldebrid":
             auth_mode = "auth_ad"
-        xbmc.executebuiltin("RunPlugin(plugin://script.module.resolveurl/?mode=%s)" % auth_mode)
-        time.sleep(20)
-        check_os = platform()
-        if check_os == 'android': 
-            open_site = xbmc.executebuiltin('StartAndroidActivity(,android.intent.action.VIEW,,%s)' % (site))
-        elif check_os == 'osx':
-           os.system("open " + site)
+        elif str(item[0]) == "premiumize":
+            auth_mode = "auth_pm"
         else:
-            open_site = webbrowser.open(site)
+            auth_mode = "auth_ls"
+        xbmc.executebuiltin("RunPlugin(plugin://script.module.resolveurl/?mode=%s)" % auth_mode)
+        if not auth_mode == "auth_ls":
+            time.sleep(20)
+            check_os = platform()
+            if check_os == 'android':
+                open_site = xbmc.executebuiltin('StartAndroidActivity(,android.intent.action.VIEW,,%s)' % (site))
+            elif check_os == 'osx':
+               os.system("open " + site)
+            else:
+                open_site = webbrowser.open(site)
+    except:
+        failure = traceback.format_exc()
+        xbmcgui.Dialog().textviewer('Exception',str(failure))
+        pass
+
+
+@route(mode="FUNCCACHE", args=["url"])
+def funccache_handler(url):
+    try:
+        plugin = ''
+        if 'funclist' in url:
+            names = []
+            for item in FUNC_LIST:
+                the_title = '[COLOR %s]Clear [COLOR %s]%s[/COLOR]' % (COLOR1, COLOR2, item[0].capitalize())
+                names.append(the_title)
+            selected = xbmcgui.Dialog().select('[COLOR %s]Select Resolver To Clear Function Cache For[/COLOR]' % COLOR2, names)
+
+            if selected ==  -1:
+                return
+
+            # If you add [COLOR] etc to the title stuff in names loop above, this will strip all of that out and make it usable here
+            rslvr_item = re.sub('\[.*?]','',names[selected]).replace('Clear ', '').lower()
+            for item in FUNC_LIST:
+                if str(item[0]) == rslvr_item:
+                    plugin = item[1]
+                    break
+        else:
+            for item in FUNC_LIST:
+                if str(item[0]) == url:
+                    plugin = item[1]
+                    break
+        if xbmc.getCondVisibility('System.HasAddon(%s)' % plugin):
+            xbmc.executebuiltin('RunPlugin(plugin://%s/?mode=reset_cache)' % plugin)
+        else:
+            xbmcgui.Dialog().notification("[COLOR %s]Clear %s Function Cache[/COLOR]" % (plugin, COLOR1), "[COLOR %s]%s not found![/COLOR]" % (plugin, COLOR2))
+
     except:
         failure = traceback.format_exc()
         xbmcgui.Dialog().textviewer('Exception',str(failure))
