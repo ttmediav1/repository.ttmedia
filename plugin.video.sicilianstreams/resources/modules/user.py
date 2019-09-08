80b197de072a8a2030688574f7e1848302a96536
ttmediav1 <eng64tw@gmail.com>
2019-09-08 16:37:51 -0700

    added sicilian updated tempest and repo
    added sicilian updated tempest and repo

---
+++ plugin.video.sicilianstreams/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLnNpY2lsaWFudHJlYW1z')
+
+name = b.b64decode('U2ljaWxpYW4gU3RyZWFtcw==')
+
+host = b.b64decode('aHR0cDovL215YWxsdmlldy5jby51aw==')
+
+port = b.b64decode('ODA=')
