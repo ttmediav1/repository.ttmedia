dcd969c4d58a91def8523b72566b9837a61e28e1
ttmediav1 <eng64tw@gmail.com>
2019-09-12 06:21:22 -0700

    added duality
    added duality

---
+++ plugin.video.duality/resources/modules/user.py
@@ -0,0 +1,13 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLmR1YWxpdHk=')
+
+name = b.b64decode('RHVhbGl0eQ==')
+
+host = b.b64decode('aHR0cDovL29udGFyZ2V0LmxpdmU=')
+
+port = b.b64decode('ODA4MA==')
+
+host1 = b.b64decode('aHR0cDovL215YWxsdmlldy5jby51aw==')
+
+port1 = b.b64decode('ODA=')
