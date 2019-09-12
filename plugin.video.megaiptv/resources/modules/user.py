3f703427252e1db46758201188388b1965c5aa87
ttmediav1 <eng64tw@gmail.com>
2019-09-12 01:59:24 -0700

    updated everything
    updated everything

---
+++ plugin.video.megaiptv/resources/modules/user.py
@@ -0,0 +1,11 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLm1lZ2FpcHR2')
+
+name = b.b64decode('TWVnYSBJUFRW')
+
+host = b.b64decode('aHR0cDovL215YWxsdmlldy5jby51aw==')
+
+port = b.b64decode('ODA=')
+
+
